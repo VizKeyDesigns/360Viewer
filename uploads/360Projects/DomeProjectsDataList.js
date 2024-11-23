f90b653b6b39398bf0ba44e81e7891faef295ae2
jmleach1987 <94634189+jmleach1987@users.noreply.github.com>
2024-11-22 19:57:31 -0600

    Test

--- uploads/360Projects/DomeProjectsDataList.js
+++ uploads/360Projects/DomeProjectsDataList.js
@@ -1,4 +1,5 @@
 // This content is dynamically generated.
 export const DomeProjectData = [
 {group:"Stores",project:"4108",dataFile:"Stores/4108/CamPos.json"}
+{group:"VizKey",project:"Test",dataFile:"VizKey/Test/CamPos.json"}
 ];
